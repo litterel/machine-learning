6388fd3241de631775408628a821fb54134679a3
litterel <litterel@hotmail.com>
2020-04-14 10:39:30 +0800

    minor modifications

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,21 +1,26 @@
 import numpy as np
 import re
 from metric import accuracy_score
+from nltk.corpus import stopwords
 
 
 class NaiveBayesClassifier():
     def __init__(self):
         self._vocab_list = None
+        self._category_list = None
+        
         self._p_c = None
         self._p_c_log = None
         self._p_word_mat = None
         self._p_word_mat_log = None
-        self._category_list = None
+        
 
     def _creat_vocab_list(self, X: np.ndarray):
         self._vocab_list = set()
         for item in X:
             self._vocab_list = self._vocab_list.union(set(item))
+        stopwords_set = set(stopwords.words('english'))
+        self._vocab_list = self._vocab_list - stopwords_set
         self._vocab_list = list(self._vocab_list)
 
     def _word2vec(self, x):
@@ -41,6 +46,10 @@ class NaiveBayesClassifier():
     '''
 
     def fit(self, X: np.ndarray, y: np.ndarray):
+        assert X.ndim == 2, "trainning data must be 2 dimensional array."
+        assert (X.shape[0] == y.shape[0]
+                ), "size of training data must be equal to training target."
+
         self._creat_vocab_list(X)
         self._category_list = np.unique(y)
         self._p_c = []
@@ -71,7 +80,3 @@ class NaiveBayesClassifier():
     def score(self, X, y):
         predictions = self.predict(X)
         return accuracy_score(y, predictions)
-
-    def text_parser(self, X):
-        for item in X:
-            pass
