2fbebdfc971c68a700146f68c5a92e615daa2bb8
litterel <litterel@hotmail.com>
2020-04-16 22:27:17 +0800

    minor modifications

--- tools.py
+++ tools.py
@@ -2,13 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def scatter_2d(X, y):
-    plt.scatter(X[y == 0, 0], X[y == 0, 1], c='r', alpha=0.7)
-    plt.scatter(X[y == 1, 0], X[y == 1, 1], c='b', alpha=0.7)
-    plt.show()
+def scatter_2d(X, y, alpha=0.6):
+    plt.scatter(X[y == 0, 0], X[y == 0, 1], c='r', alpha=alpha)
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], c='b', alpha=alpha)
 
 
-def plot_boundry(algo, axis, poly_algo=None):
+def plot_boundry(algo, axis, poly_algo=None, alpha=0.5):
     XX, YY = np.meshgrid(
         np.linspace(axis[0], axis[1], int((axis[1] - axis[0]) * 100)),
         np.linspace(axis[2], axis[3], int((axis[3] - axis[2]) * 100)))
@@ -18,6 +17,5 @@ def plot_boundry(algo, axis, poly_algo=None):
         plot_X = poly_algo.transform(plot_X)
     y = algo.predict(plot_X)
     y = y.reshape(XX.shape)
-    plt.contourf(XX, YY, y, cmap = plt.cm.winter, alpha=0.75)
-    plt.show()
+    plt.contourf(XX, YY, y, cmap=plt.cm.winter, alpha=alpha)
 
